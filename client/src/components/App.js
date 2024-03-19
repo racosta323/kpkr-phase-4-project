7fae635ddf92c33f2de4b7c0289f7480b5c02232
stacks-loops <p.cronin.connolly@gmail.com>
2024-03-19 15:53:57 -0400

    image line added to onboard

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
+  <img className="onboarding-container" src="" alt="" />
   return <h1>Welcome to NestEgg!</h1>;
 }
 
