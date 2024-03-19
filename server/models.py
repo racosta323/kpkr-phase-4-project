ef3a4bf23db2cb20a1aea1be31173d3f3fd7978a
Rene <racosta323@gmail.com>
2024-03-19 15:00:40 -0500

    generates table and adds seed data

--- server/models.py
+++ server/models.py
@@ -4,3 +4,16 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    account_balance = db.Column(db.Integer, nullable=False)
+    gross = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<User Id: {self.id}, first_name = {self.first_name}, last_name = {self.last_name}>'
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,26 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+        print('Deleting Data....')
+        User.query.delete()
+
+        users = []
+
+        for n in range(5):
+            user = User(
+                first_name=fake.first_name(),
+                last_name = fake.last_name(),
+                account_balance=fake.pricetag()
+                )
+            users.append(user)
+            
+        print("Adding users...")    
+        db.session.add_all(users)    
+        db.session.commit()
