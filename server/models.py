0f6fc46245c955c7be25103e7a5e19bc5e695b30
Rene <racosta323@gmail.com>
2024-03-19 15:10:07 -0500

    inits users table and adds seed data

--- server/models.py
+++ server/models.py
@@ -16,4 +16,4 @@ class User(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     def __repr__(self):
-        return f'<User Id: {self.id}, first_name = {self.first_name}, last_name = {self.last_name}>'
+        return f'<User Id: {self.id}, first_name = {self.first_name}, last_name = {self.last_name}, account_balance = {self.account_balance}>'
