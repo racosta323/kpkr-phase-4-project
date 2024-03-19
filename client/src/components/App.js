eb6e16ba151b395cbc3a88ebf2462d89602e3527
stacks-loops <p.cronin.connolly@gmail.com>
2024-03-19 16:25:07 -0400

    some styling added to onboarding, form placeholder

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,8 +2,24 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  <img className="onboarding-container" src="" alt="" />
-  return <h1>Welcome to NestEgg!</h1>;
+  return (
+    <div className='app-container'>
+      <img className="onboarding-container" src="" alt="" />
+       <h1>Welcome to Finast</h1>
+        <h2>Set your savings goals and track your progress!</h2>
+
+        <div className='form-box'>
+         
+        </div>
+
+        <p> 
+          Savings. Made. Simple. 
+          In a technical world full of options and confusion we want to simplify for you. 
+          Decide what you want, and track your progress toward that goal.
+        </p>
+    </div>
+  
+  )
 }
 
 export default App;
