b82b6722dfd58d19253eac48809215834b099ceb
Rene <racosta323@gmail.com>
2024-03-20 11:44:55 -0500

    updates tables based on new ERD

--- server/models.py
+++ server/models.py
@@ -10,21 +10,20 @@ class User(db.Model, SerializerMixin):
     id=db.Column(db.Integer, primary_key=True)
     first_name=db.Column(db.String)
     last_name=db.Column(db.String)
-    account_balance=db.Column(db.Integer, nullable=False)
-    gross=db.Column(db.Integer)
     created_at=db.Column(db.DateTime, server_default=db.func.now())
     updated_at=db.Column(db.DateTime, onupdate=db.func.now())
 
     def __repr__(self):
-        return f'<User Id: {self.id}, first_name = {self.first_name}, last_name = {self.last_name}, account_balance = {self.account_balance}>'
+        return f'<User Id: {self.id}, first_name = {self.first_name}, last_name = {self.last_name}>'
     
 
-class Income(db.Model, SerializerMixin):
-    __tablename__ = 'incomes' 
+class UserGoal(db.Model, SerializerMixin):
+    __tablename__ = 'user_goals' 
 
     id=db.Column(db.Integer, primary_key=True)
-    amount=db.Column(db.Integer, nullable=False)
-    frequency=db.Column(db.String, nullable=False)
+    contributions = db.Column(db.Integer, nullable=False)
+    progress=db.Column(db.Integer)
+    completed_date=db.Column(db.DateTime, nullable=False)
     created_at=db.Column(db.DateTime, server_default=db.func.now())
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
@@ -32,29 +31,17 @@ class Income(db.Model, SerializerMixin):
     def __repr__(self):
         return f'<Income ID: {self.id}, Income amount: {self.amount}, Income frequency: {self.frequency}, User ID: {self.user_id} >'
     
-class Expense(db.Model, SerializerMixin):
-    __tablename__ = 'expenses' 
-
-    id=db.Column(db.Integer, primary_key=True)
-    amount=db.Column(db.Integer, nullable=False)
-    frequency=db.Column(db.String, nullable=False)
-    created_at=db.Column(db.DateTime, server_default=db.func.now())
-
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-
-    def __repr__(self):
-        return f'<Expenses ID: {self.id}, Expenses amount: {self.amount}, Expenses frequency: {self.frequency}, User ID: {self.user_id} >'
     
 class Goal(db.Model, SerializerMixin):
     __tablename__ = 'goals' 
 
     id=db.Column(db.Integer, primary_key=True)
     amount=db.Column(db.Integer, nullable=False)
-    goal=db.Column(db.String, nullable=False)
+    name=db.Column(db.String, nullable=False)
     target_date=db.Column(db.DateTime)
     created_at=db.Column(db.DateTime, server_default=db.func.now())
 
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    goal_id = db.Column(db.Integer, db.ForeignKey("user_goals.id"))
 
     def __repr__(self):
         return f'<Goal ID: {self.id}, Goal amount: {self.amount}, Goal: {self.frequency}, User ID: {self.user_id} >'
