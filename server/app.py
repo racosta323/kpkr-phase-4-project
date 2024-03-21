91ad7643e189a57ca2862cf9e85e166091ee8d5d
Rene <racosta323@gmail.com>
2024-03-21 11:51:37 -0500

    adds post to users

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
@@ -16,6 +16,16 @@ from models import User, Goal, UserGoal
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+class Users(Resource):
+    def post(self):
+        new_user = User(first_name=request.get_json()["first_name"], last_name=request.get_json()["last_name"])
+        db.session.add(new_user)
+        db.session.commit()
+
+        return make_response(new_user.to_dict(), 201)
+
+api.add_resource(Users, '/users')
 
 
 if __name__ == '__main__':
