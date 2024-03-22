7e438bc15e6ef745e7c1b72e643684b526630203
kent riggs <k2r3@proton.me>
2024-03-21 17:45:04 -0700

    added logo

--- client/src/components/Onboarding.js
+++ client/src/components/Onboarding.js
@@ -7,7 +7,7 @@ function Onboarding() {
 
   return (
     <div className='app-container'>
-      <img className="onboarding-container" src="" alt="" />
+      <img className="onboarding-container" src="https://cdn.discordapp.com/attachments/1102300163525058591/1220487914069426328/F2-2.png?ex=660f1ef2&is=65fca9f2&hm=0f2abb8af89c958a4533d5bf22e821e2e659e27c6190be46a212331a5a65db47&" alt="F2 logo" style={{ width: '100px', height: 'auto' }}/>
        <h1> 💎💎💎 Welcome to FridayFund 💎💎💎</h1>
         <h2>Set your savings goals and track your progress!</h2>
         <NavBar/>
@@ -26,3 +26,4 @@ function Onboarding() {
 }
 
 export default Onboarding;
+
