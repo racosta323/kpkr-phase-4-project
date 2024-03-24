72812060a6e16e65eebbc3a8499460d74b07f873
Rene <racosta323@gmail.com>
2024-03-24 17:38:48 -0500

    refactors code to allow edit on name alone

--- client/src/components/Confirmation.js
+++ client/src/components/Confirmation.js
@@ -12,6 +12,7 @@ import FakePage from './FakePage'
 function Contributions({ formik, click }){
 
   const [edit, setEdit] = useState("")
+  const [nameEdit, setNameEdit] = useState("")
 
     console.log(formik)
 
@@ -45,19 +46,50 @@ function Contributions({ formik, click }){
     }
 
 
-    function editClick(){
-      setEdit("editName")
+    function nameClick(){
+      setNameEdit("edit")
     }
 
-
-    
-
+  
     const toggle = () => {
       if (edit === ""){
         return(
           <>
-            {options('firstName')}
-            {options('lastName')}
+            <Row>
+              <Col></Col>
+              <Col className="border border-dark d-flex justify-content-center h-100 pt-3">
+                <Form className="w-75 m-5"> 
+                  <Form.Group>
+                    <Form.Label className="fs-2 mb-4">Confirm your details</Form.Label>
+                    <Stack direction='horizontal' gap={3}>
+                      <h3 >Name</h3>
+                      <Button as="input" className='ms-auto w-25' value="Edit" />
+                    </Stack>
+                    {options('firstName')}
+                    {options('lastName')}
+                    <Stack direction='horizontal' gap={3}>
+                      <h3 >Goals</h3>
+                      <Button as="input" className='ms-auto w-25' value="Edit" onClick/>
+                    </Stack>
+                    {options('goalAmt')}
+                    {options('goalName')}
+                    {options('contributions')}
+                    <Stack direction='horizontal' gap={3}>
+                      <h3 >Contributions</h3>
+                      <Button as="input" className='ms-auto w-25' value="Edit" onClick/>
+                    </Stack>
+                    {options()}
+                    {/* consider adding ability to add another*/}
+                    <Stack direction='horizontal' gap={3}>
+                      <Button as="input" type="button" value="Next" onClick={click}/>{' '}
+                      <a href="/fakepage" className='ms-auto'>Skip</a>
+                    </Stack>
+                  </Form.Group>
+                </Form>
+              </Col>
+              <Col></Col>
+            </Row>
+            <Row></Row>
           </>
         )
       } else if (edit === "editName") {
@@ -71,42 +103,48 @@ function Contributions({ formik, click }){
       }
     }
 
+
+const nameStack = () => {
+  if (nameEdit === ""){
+    return (
+      <>
+        <Stack direction='horizontal' gap={3}>
+          <h3 >Name</h3>
+          <Button as="input" className='ms-auto w-25' value="Edit" onClick={nameClick}/>
+        </Stack>
+        {options('firstName')}
+        {options('lastName')}
+      </>
+    )
+  } else if (nameEdit==="edit"){
+    return(
+      <>
+        <Stack direction='horizontal' gap={3}>
+          <h3 >Name</h3>
+        </Stack>
+        {editOptions('firstName')}
+        {editOptions('lastName')}
+      </>
+    )
+  }
+}
+
 return(
     <>
-    <Row>
-      <Col></Col>
-      <Col className="border border-dark d-flex justify-content-center h-100 pt-3">
-        <Form className="w-75 m-5"> 
-          <Form.Group>
-            <Form.Label className="fs-2 mb-4">Confirm your details</Form.Label>
-            <Stack direction='horizontal' gap={3}>
-              <h3 >Name</h3>
-              <Button as="input" className='ms-auto w-25' value="Edit" onClick={editClick}/>
-            </Stack>
-
-            {toggle()}
-
-            <Stack direction='horizontal' gap={3}>
-              <h3 >Name</h3>
-              <Button as="input" className='ms-auto w-25' value="Edit" onClick/>
-            </Stack>
-            <Stack direction='horizontal' gap={3}>
-              <h3 >Name</h3>
-              <Button as="input" className='ms-auto w-25' value="Edit" onClick/>
-            </Stack>
-            {/* consider adding ability to add another*/}
-            <Stack direction='horizontal' gap={3}>
-              <Button as="input" type="button" value="Next" onClick={click}/>{' '}
-              <a href="/fakepage" className='ms-auto'>Skip</a>
-            </Stack>
-            
-            
-          </Form.Group>
-        </Form>
-      </Col>
-      <Col></Col>
-    </Row>
-    <Row></Row>
+      <Row>
+        <Col></Col>
+        <Col className="border border-dark d-flex justify-content-center h-100 pt-3">
+          <Form className="w-75 m-5"> 
+            <Form.Group>
+              <Form.Label className="fs-2 mb-4">Confirm your details</Form.Label>
+              {nameStack()}
+            </Form.Group>
+          </Form>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row></Row>
+      {/* {toggle()} */}
     </>
 )
 
