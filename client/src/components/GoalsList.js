9cb257e525fe644d006c0e151a679ad7715677e4
Rene <racosta323@gmail.com>
2024-03-25 17:09:32 -0500

    updates how goal appears on page -- GoalsList

--- client/src/components/GoalsList.js
+++ client/src/components/GoalsList.js
@@ -1,10 +1,20 @@
 import Container from "react-bootstrap/Container"
+import ProgressBar from "react-bootstrap/ProgressBar"
+import Stack from 'react-bootstrap/Stack'
 
 function GoalsList(){
     return (
         <Container>
             <div class="list-group">
-                <a href="#" class="rounded border border-primary-subtle list-group-item list-group-item-action mb-4">The current link item</a>
+                
+                <a href="#" class="rounded border border-primary-subtle list-group-item list-group-item-action mb-4">
+                    <Stack direction="horizontal" gap={3}>
+                        <h4>Goal</h4>
+                        <h4 className="ms-auto">$$$$</h4>
+                    </Stack>
+                    <p>This is a goal</p>
+                    <ProgressBar now={60}/>
+                </a>
                 <a href="#" class="rounded border border-primary-subtle list-group-item list-group-item-action mb-4">A second link item</a>
                 <a href="#" class="rounded border border-primary-subtle list-group-item list-group-item-action mb-4">The current link item</a>
             </div>
