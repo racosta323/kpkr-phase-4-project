0636bca66ea355cf067c7ee1cb5a51170c5f077e
Rene <racosta323@gmail.com>
2024-03-26 10:10:41 -0500

    resizes and centers chart

--- client/src/components/Goal.js
+++ client/src/components/Goal.js
@@ -2,14 +2,12 @@ import React from "react";
 import { useState } from 'react'
 
 import EditModal from "./EditModal";
-
 import NavBar from "./NavBar";
+
 import Graph from "./Graph";
 import Row from "react-bootstrap/Row"
-import Column from "react-bootstrap/Col"
-import Modal from 'react-bootstrap/Modal'
-import Button from "react-bootstrap/Button";
-
+import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container";
 
 function Goal() {
     const goalAmount = 15000
@@ -24,13 +22,17 @@ function Goal() {
     <>
       <NavBar/>
       <Row className="m-4"></Row>
-      <Row>
-        <Column>
-          <Graph goalAmount={goalAmount} userContributions={userContributions} />
-          <button className="edit-button btn btn-secondary" onClick={handleShow}>Update your goal</button>
-        </Column>
-      </Row>
+      <Container fluid className="d-flex justify-content-center">
+        <Row className="w-25">
+          <Col></Col>
+          <Col>
+            <Graph goalAmount={goalAmount} userContributions={userContributions} />
+            <button className="edit-button btn btn-secondary" onClick={handleShow}>Update your goal</button>
+          </Col>
+          <Col></Col>
+        </Row>
       <EditModal show={show} handleClose={handleClose}/>
+      </Container>
     </>
       // <div className='app-container'>
       //   <NavBar/>
