1abebfd40ce181961a05e263095cd6326ffb281d
Rene <racosta323@gmail.com>
2024-03-26 10:28:10 -0500

    fixes goals list nav

--- client/src/components/GoalsList.js
+++ client/src/components/GoalsList.js
@@ -3,13 +3,12 @@ import ProgressBar from "react-bootstrap/ProgressBar"
 import Stack from 'react-bootstrap/Stack'
 
 function GoalsList({ name, amount, contributions, id }){
-    let href = `goals/${id}`
 
     return (
         <Container>
             <div class="list-group">
                 
-                <a href = {href} class="rounded border border-primary-subtle list-group-item list-group-item-action mb-4">
+                <a href={id} class="rounded border border-primary-subtle list-group-item list-group-item-action mb-4">
                     <Stack direction="horizontal" gap={3}>
                         <h4>Goal</h4>
                         <h4 className="ms-auto">${contributions} of ${amount} raised</h4>
