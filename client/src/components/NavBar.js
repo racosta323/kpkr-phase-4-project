2696da1d20c86157e2345daf6a6cbe9dbaec1b69
Rene <racosta323@gmail.com>
2024-03-28 02:48:39 -0500

    fixes logout button in nav

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,9 +3,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button'
-import { NavLink } from "react-router-dom";
+import { NavLink, useOutletContext } from "react-router-dom";
 
-function NavBar( { logoutUser }) {
+
+function NavBar( ) {
+
+  const { loggedInUser, setLoggedInUser, logoutUser } = useOutletContext()
 
   function handleLogout() {
     fetch('/logout', {
