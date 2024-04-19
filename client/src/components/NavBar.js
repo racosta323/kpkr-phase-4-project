c7d20886658f8507d7682d723222c4bd0c9e7109
Rene <racosta323@gmail.com>
2024-04-19 15:50:04 -0500

    removes image from navbar -- image not available

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -24,11 +24,6 @@ function NavBar( ) {
     return(
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/"> <img src="https://cdn.discordapp.com/attachments/1102300163525058591/1220789201507848253/F2-3.png?ex=6610378a&is=65fdc28a&hm=d762b3e82ba9352179982cf5867ca00401239700edd5f46040a08c412f79a46b&" 
-            width="45" 
-            height="45" 
-            className="d-inline-block align-top" 
-            alt="F2 logo" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
