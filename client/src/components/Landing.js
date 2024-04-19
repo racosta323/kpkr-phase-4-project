1cc88e49246f89a0d5007065839af6d1956a42b6
Rene <racosta323@gmail.com>
2024-04-19 15:41:51 -0500

    fixes landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -11,38 +11,40 @@ function Landing(){
 
     return(
       <>
-        <Container fluid>
-          <Row className="m-4"></Row>
+        <Container fluid className="p-5">
+          <Row className="my-4"></Row>
+          <Row className="my-4"></Row>
+          <Row className="my-4"></Row>
+          <Row className="my-4"></Row>
+          <Row className="my-4"></Row>
+          <Row className="my-4"></Row>
           <Row className="d-flex justify-content-center">
-            <Col></Col>
+            
             <Col xs={6}>
-                <img className="rounded mx-auto d-block" src="https://cdn.discordapp.com/attachments/1102300163525058591/1220789201507848253/F2-3.png?ex=6610378a&is=65fdc28a&hm=d762b3e82ba9352179982cf5867ca00401239700edd5f46040a08c412f79a46b&" alt="F2 logo" style={{ width: '100px', height: 'auto' }}/>
                 <h1 className="text-center mt-4"> 💎💎💎 FridayFund 💎💎💎</h1>
                 <h2 className="text-center">Savings goals made simple</h2>
             </Col>
-            <Col></Col>
+
           </Row>
 
           <Row className="m-4"></Row>
 
-          <Row className="m-5">
-            <Col></Col>
+          <Row className="d-flex justify-content-center m-5 ">
+           
               <Col xs={6}>
               <h4 className="text-center">Perception is reality. By having a simple mechanism to track and visualize your savings goals it becomes easier and even encouraging to save up for what you desire most.</h4>
               </Col>
-              <Col></Col>
+      
           </Row>
-          <Row>
-            <Col></Col>
+          <Row className="d-flex justify-content-center">
             <Col xs={6} className="d-flex justify-content-center">
               <Button value="Login" href='/login' className="me-2">Login</Button>
               <Button value="Sign Up" href='/signup'>Sign Up</Button>
             </Col>
-            <Col></Col>
           </Row>
           
           <Row className="m-4"></Row>
-          <Row>
+          <Row className="d-flex justify-content-center">
             <p className="text-center"> 
               Create an account or sign in. 
               Add a goal to your profile, submit your desired amount, watch your savings grow!
